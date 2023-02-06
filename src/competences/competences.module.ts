b258d2a5699261f6e867c28b683575a3604cb702
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-06 13:29:53 +0100

    feat competences.module.ts

--- src/competences/competences.module.ts
+++ src/competences/competences.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CompetencesService } from './competences.service';
 import { CompetencesController } from './competences.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Competence } from './entities/competence.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Competence])],
   controllers: [CompetencesController],
-  providers: [CompetencesService]
+  providers: [CompetencesService],
+  exports: [Competence]
 })
 export class CompetencesModule {}
