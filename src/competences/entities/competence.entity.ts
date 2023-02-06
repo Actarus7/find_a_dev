2ecd2c93fb3e3d198ea8f78b74c8f1d2e392db4d
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-06 11:11:26 +0100

    feat competence.entity.ts

--- src/competences/entities/competence.entity.ts
+++ src/competences/entities/competence.entity.ts
@@ -1,1 +1,11 @@
-export class Competence {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('competences')
+export class Competence extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    competence: string
+}
