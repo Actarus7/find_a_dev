6ab8a38ab45784d3862cbc59085c9519dbff11f0
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-06 11:26:15 +0100

    feat presentation.entity.ts

--- src/presentations/entities/presentation.entity.ts
+++ src/presentations/entities/presentation.entity.ts
@@ -1,1 +1,14 @@
-export class Presentation {}
+import { Profile } from "src/profiles/entities/profile.entity";
+import { BaseEntity, Column, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export class Presentation extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    description: string
+
+    @OneToOne(() => Profile)
+    profile: Profile
+}
