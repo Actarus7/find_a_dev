f67ac782cd6f8ab3da69a11cb401cf606f23fe59
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-06 11:27:21 +0100

    feat : Ajout Entity friendship

--- src/friendships/entities/friendship.entity.ts
+++ src/friendships/entities/friendship.entity.ts
@@ -1,1 +1,22 @@
-export class Friendship {}
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
+
+
+/**
+ * Entity repressentant les liaison entre les users
+ */
+@Entity('friendships')
+export class Friendship {
+
+    /** Le Friend a-t-il accepté la demande */
+    @Column()
+    allowed: boolean
+
+    /** Le user émetteur de la demande*/
+    @ManyToOne(() => User, (user) => user.friendships)
+    user : User
+
+    /** Le user resepteur de la demande*/
+    @ManyToOne(() => User, (user) => user.friends)
+    friend : User
+}
