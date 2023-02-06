5b6c28c3976392f2157d86e9acbfc71601b3a6cb
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-06 11:44:15 +0100

    competence.dto

--- src/competences/dto/create-competence.dto.ts
+++ src/competences/dto/create-competence.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCompetenceDto {}
+import { PartialType } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { IsNotEmpty } from "class-validator/types/decorator/decorators";
+import { UpdateCompetenceDto } from "./update-competence.dto";
+
+export class CreateCompetenceDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    
+    competence: string
+}
+
--- src/competences/dto/update-competence.dto.ts
+++ src/competences/dto/update-competence.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator/types/decorator/decorators';
 import { CreateCompetenceDto } from './create-competence.dto';
 
-export class UpdateCompetenceDto extends PartialType(CreateCompetenceDto) {}
+export class UpdateCompetenceDto extends PartialType(CreateCompetenceDto) {
+
+    @IsString()
+    @IsNotEmpty()
+    
+    competence: string
+}
+
