247d539bcd498c9bffba0295a4b9450f228a9a8f
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-06 11:31:53 +0100

    @APIProperty

--- src/competences/entities/competence.entity.ts
+++ src/competences/entities/competence.entity.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Profile } from "src/profiles/entities/profile.entity";
 import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity('competences')
 export class Competence extends BaseEntity{
+
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number
 
+    @ApiProperty()
     @Column()
     competence: string
 
+    @ApiProperty()
     @ManyToOne(() => Profile, (profile) => profile.competence)
     profile: Profile
 }
--- src/presentations/entities/presentation.entity.ts
+++ src/presentations/entities/presentation.entity.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Profile } from "src/profiles/entities/profile.entity";
 import { BaseEntity, Column, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 export class Presentation extends BaseEntity{
 
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number
 
+    @ApiProperty()
     @Column()
     description: string
 
+    @ApiProperty()
     @OneToOne(() => Profile)
     profile: Profile
 }
