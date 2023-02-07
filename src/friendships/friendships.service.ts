5aa272a948234921ce63bc99d8d7cd28263bb58a
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-07 11:43:54 +0100

    feat : FriendshipsService.create

--- src/friendships/friendships.service.ts
+++ src/friendships/friendships.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFriendshipDto } from './dto/create-friendship.dto';
-import { UpdateFriendshipDto } from './dto/update-friendship.dto';
+import { User } from 'src/users/entities/user.entity';
+import { Friendship } from './entities/friendship.entity';
 
+
+/**
+ * Essemble des services pour la table friendships :
+ * 
+ * * **create**               : Permet la création de nouvelle relation entre 2 user
+ * * **findOne**              : Permet de trouver la relation user => friend
+ * * **findBoth**             : Permet de trouver les relations user => friend et friend => user
+ * * **findStatusWithObject** : Permet d'obtenir le statut d'une relation en fonction des pseudos de 2 user
+ * * **findStatusWithPseudo** : Permet d'obtenir le statut d'une relation en fonction des pseudos de 2 user
+ * * **promote**              : Permet de valider l'état d'une relation
+ * * **remove**               : 
+ */
 @Injectable()
 export class FriendshipsService {
-  create(createFriendshipDto: CreateFriendshipDto) {
-    return 'This action adds a new friendship';
-  }
 
-  findAll() {
-    return `This action returns all friendships`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} friendship`;
-  }
-
-  update(id: number, updateFriendshipDto: UpdateFriendshipDto) {
-    return `This action updates a #${id} friendship`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} friendship`;
+  /**
+   * Permet la création de nouvelle relation entre 2 user
+   * 
+   * @param user      Objet du demandeur
+   * @param friend    Objet de la cible
+   * @returns         Les 2 nouvelles ligne crées :
+   * * la première sur true
+   * * la seconde sur false (en attente de validation de la cible)
+   */ 
+  async create(user : User, friend : User) {
+    return [
+      await Friendship.create({
+        allowed : true ,
+        user : user ,
+        friend : friend
+      }).save(),
+      await Friendship.create({
+        allowed : false ,
+        user : friend ,
+        friend : user
+      }).save(),
+    ]
   }
 }
