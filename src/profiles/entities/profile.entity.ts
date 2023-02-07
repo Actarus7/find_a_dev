81f85c9cf2656204eaefb7a8525b3bacc934693e
kevinbatard <kevin.batard4064@gmail.com>
2023-02-07 14:07:13 +0100

    feat commentaire de auth controller, et user controller

--- src/profiles/entities/profile.entity.ts
+++ src/profiles/entities/profile.entity.ts
@@ -1,9 +1,18 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Competence } from "src/competences/entities/competence.entity";
-import { Language } from "src/languages/entities/language.entity";
-import { Presentation } from "src/presentations/entities/presentation.entity";
-import { User } from "src/users/entities/user.entity";
-import { BaseEntity, Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
+import { Competence } from 'src/competences/entities/competence.entity';
+import { Language } from 'src/languages/entities/language.entity';
+import { Presentation } from 'src/presentations/entities/presentation.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('profiles')
 export class Profile extends BaseEntity {
@@ -17,17 +26,16 @@ export class Profile extends BaseEntity {
   presentation: Presentation;
 
   @ApiProperty()
-  @ManyToMany(() => Language, language => language.profiles)
+  @ManyToMany(() => Language, (language) => language.profiles)
   @JoinTable()
   languages: Language[];
 
   @ApiProperty()
-  @OneToMany(() => Competence, competence => competence.profile)
+  @OneToMany(() => Competence, (competence) => competence.profile)
   competences: Competence[];
 
-  @ApiProperty({type : () => User})
+  @ApiProperty({ type: () => User })
   @JoinColumn()
   @OneToOne(() => User)
   user: User;
-
-};
+}
