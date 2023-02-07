739338b7195742cc0c271548edeb681bc3b61c45
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-02-07 15:15:24 +0100

    feat : FriendshipsController.findOne

--- src/friendships/friendships.controller.ts
+++ src/friendships/friendships.controller.ts
@@ -3,22 +3,26 @@ import {
   Get,
   Post,
   Request,
-  Patch,
   Param,
   Delete,
   ConflictException,
-  ParseIntPipe,
   NotFoundException,
 } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common/decorators';
-import { Bind } from '@nestjs/common/decorators/core/bind.decorator';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { FriendshipsService } from './friendships.service';
 
-/**décorateur Tag permettant de catégoriser les différentes route dans la doc API Swagger*/
+/**
+ * Essemble des controlers pour la table friendships :
+ * 
+ * * **create**   : Création ou mise à jour d'une Friendship
+ * * **findOne**  : Récupération du status d'une Friendship
+ * * **findall**  : Ensemble des relation d'un user
+ * * **remove**   : Permet la suppression des liaisons entre 2 users
+ */
+//décorateur Tag permettant de catégoriser les différentes route dans la doc API Swagger
 @ApiTags('Friendships')
 @Controller('friendships')
 export class FriendshipsController {
@@ -30,8 +34,8 @@ export class FriendshipsController {
   /**
    * Création ou mise à jour d'une Friendship
    *
-   * @param friendPseudo Nom du user que l'on souhaite lier
-   * @param req La requete contenant le token
+   * @param friendPseudo  Nom du user que l'on souhaite lier
+   * @param req           La requete contenant le token
    * @returns le statut actuel du lien :
    * * 0 : N'existe pas
    * * 1 : En attente de votre part
@@ -53,10 +57,7 @@ export class FriendshipsController {
       throw new NotFoundException('This pseudo does not exist');
     }
 
-    let status = await this.friendshipsService.findStatusWithPseudo(
-      userPseudo,
-      friendPseudo,
-    );
+    let status = await this.friendshipsService.findStatusWithPseudo( userPseudo, friendPseudo );
 
     if (status.code === 0) {
       const user = await this.usersService.findOneByPseudo(userPseudo);
@@ -78,27 +79,39 @@ export class FriendshipsController {
     };
   }
 
-  @Get('/:friendPseudo')
-  async findOne(@Param('friendPseudo') friendPseudo: string) {
-    const userPseudo = 'jo';
-    return await this.friendshipsService.findOne(userPseudo, friendPseudo);
-  }
-  /*
-  @Get()
-  async findall(@Param('friendPseudo') friendPseudo: string) {
-    return await this.friendshipsService.findAll()
-  }
-  */
-  /*
+  /**
+   * Récupération du status d'une Friendship
+   *
+   * @param friendPseudo  Nom du user que l'on souhaite lier
+   * @param req           La requete contenant le token
+   * @returns le statut actuel du lien :
+   * * 0 : N'existe pas
+   * * 1 : En attente de votre part
+   * * 2 : En attente de la part de l'autre user
+   * * 3 : Le lien est partagé
+   */
+  @UseGuards(new JwtAuthGuard())
+  @Get(':friendPseudo')
+  async findOne(@Param('friendPseudo') friendPseudo: string, @Request() req) {
+    const userPseudo = req.user.pseudo;
+    const friend = await this.usersService.findOneByPseudo(friendPseudo);
 
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.friendshipsService.update(+id);
+    if (friend === null) {
+      throw new NotFoundException('This pseudo does not exist');
+    }
+    
+    const status = await this.friendshipsService.findStatusWithPseudo(
+      userPseudo,
+      friendPseudo,
+    );
+    return {
+      statusCode: 200,
+      message: 'Friendship status',
+      data: {
+        users: [userPseudo, friendPseudo],
+        status: status.status,
+      },
+    };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.friendshipsService.remove(+id);
-  }
-  */
 }
