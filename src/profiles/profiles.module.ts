25d8f1b02390107dab4f56e86b53e87118dcb176
kevinbatard <kevin.batard4064@gmail.com>
2023-02-10 14:03:11 +0100

    feat ajout longitude latitude a la création d'un user via api externe geocode + suppression imports useless et console log

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -5,9 +5,17 @@ import { LanguagesService } from 'src/languages/languages.service';
 import { CompetencesService } from 'src/competences/competences.service';
 import { PresentationsService } from 'src/presentations/presentations.service';
 import { UsersService } from 'src/users/users.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [ProfilesController],
-  providers: [ProfilesService, LanguagesService, CompetencesService, PresentationsService, UsersService]
+  providers: [
+    ProfilesService,
+    LanguagesService,
+    CompetencesService,
+    PresentationsService,
+    UsersService,
+  ],
 })
 export class ProfilesModule {}
